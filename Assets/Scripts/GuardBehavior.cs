7dfc1d95cf34c7b5c0d25e31479075bcfa79744c
Henry Luengas <gyatso@pop-os.localdomain>
2020-02-05 20:02:41 -0800

    navmesh

--- Assets/Scripts/GuardBehavior.cs
+++ Assets/Scripts/GuardBehavior.cs
@@ -1,13 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class GuardBehavior : MonoBehaviour
 {
     public GameObject guardEye;
 
     public GameObject[] patrolPath;
+    public NavMeshAgent guardAgent;
     // Start is called before the first frame update
+
+    private bool isPatroling;
+    private bool patrolTargetReached;
+    private bool isInvestigating;
+    private bool investigationTargetReached;
+    
     void Start()
     {
         

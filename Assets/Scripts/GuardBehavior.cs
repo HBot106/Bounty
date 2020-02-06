1579b028101ae5fa42f73cc3e5a437bdb2acfa2c
Henry Luengas <gyatso@pop-os.localdomain>
2020-02-05 19:46:25 -0800

    init

---
+++ Assets/Scripts/GuardBehavior.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuardBehavior : MonoBehaviour
+{
+    public GameObject guardEye;
+
+    public GameObject[] patrolPath;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

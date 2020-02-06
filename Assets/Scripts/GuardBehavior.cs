a38e5df3770094cebeabf63fc9f70c8e2ecc7de0
Henry Luengas <gyatso@pop-os.localdomain>
2020-02-05 20:45:20 -0800

    patrol route

--- Assets/Scripts/GuardBehavior.cs
+++ Assets/Scripts/GuardBehavior.cs
@@ -12,18 +12,44 @@ public class GuardBehavior : MonoBehaviour
     // Start is called before the first frame update
 
     private bool isPatroling;
-    private bool patrolTargetReached;
-    private bool isInvestigating;
-    private bool investigationTargetReached;
-    
+    private int patrolPathIndex;
+    private float stopTime;
+    private float elapsedTime;
+
     void Start()
     {
-        
+        isPatroling = true;
+        patrolPathIndex = 0;
+        stopTime = Time.time;
+        guardAgent.SetDestination(patrolPath[patrolPathIndex].transform.position);
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        elapsedTime = Time.time - stopTime;
+        if (guardAgent.pathPending)
+        {
+            return;
+        }
+
+        if (guardAgent.remainingDistance <= guardAgent.stoppingDistance)
+        {
+            if (isPatroling)
+            {
+                stopTime = Time.time;
+                patrolPathIndex = patrolPathIndex + 1 % patrolPath.Length;
+            }
+            else
+            {
+                //attack player you have reached them
+            }
+        }
+
+        if (isPatroling && (elapsedTime > 1.0f))
+        {
+            guardAgent.SetDestination(patrolPath[patrolPathIndex].transform.position);
+        }
+
     }
 }

9704c50792bfa64d1c094673b45c4c0e6f5f854e
Neon <quang.neon.le@gmail.com>
2020-02-19 15:40:32 -0800

    merged Throwables

--- Assets/Scripts/GuardBehavior.cs
+++ Assets/Scripts/GuardBehavior.cs
@@ -319,5 +319,35 @@ public class GuardBehavior : MonoBehaviour
         {
             swingState = 1;
         }
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PlayerSword"))
+        {
+            if(isPatroling)
+            {
+                Debug.Log("Guard Assassinated!");
+                Destroy(gameObject);
+            }
+            else
+            {
+                Debug.Log("Guard Hit!");
+
+                Vector3 hitDirection = (transform.position - other.transform.root.transform.position).normalized + Vector3.up;
+                rgdbdy.AddForce(hitDirection * playerHitForce, ForceMode.Impulse);
+                life--;
+                if(life == 0)
+                {
+                    Debug.Log("Guard Killed!");
+                    Destroy(gameObject);
+                }
+            }
+        }
+
+        if (other.gameObject.CompareTag("PlayerKnifeProjectile"))
+        {
+            Destroy(gameObject);
+        }
     }
 }

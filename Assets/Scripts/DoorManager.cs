0ba6476ef146e15f13fe58d45c0716858af560b5
aehoover <ashoover@calpoly.edu>
2020-02-27 11:23:34 -0800

    doors can now open correctly, depending on their orientation

--- Assets/Scripts/DoorManager.cs
+++ Assets/Scripts/DoorManager.cs
@@ -4,6 +4,17 @@ using UnityEngine;
 
 public class DoorManager : MonoBehaviour
 {
+    public GameObject player;
+    public GameObject thisDoor;
+    public float transformXVal = 0;
+    public float transformYVal = 0;
+    public float transformZVal = 0;
+    private bool encountered_locked_door = false;
+    private string locked_text = "The door is locked.";
+    private int lock_window_width = 400;
+    private int lock_window_height = 200;
+    public GUIStyle lockStyle;
+    private bool door_is_open = false;
     public bool door_is_locked = true;
 
     // Start is called before the first frame update
@@ -16,5 +27,63 @@ public class DoorManager : MonoBehaviour
     void Update()
     {
         
+    }
+    void OnGUI()
+    {
+        int windowXVal = ( Screen.width / 2 - lock_window_width / 2 );
+        int windowYVal = ( Screen.height / 2 - lock_window_height / 2 );
+
+        if ( encountered_locked_door )
+        {
+            GUI.Box( new Rect( windowXVal, windowYVal, lock_window_width, lock_window_height ), locked_text );
+
+            if ( player.GetComponent<InventoryManager>().num_keys > 0 )
+            {
+                if ( GUI.Button( new Rect( windowXVal + 10, windowYVal + 50, 100, 50 ), "Ulock" ) )
+                {
+                    print( "Click!" );
+                    encountered_locked_door = false;
+                    door_is_locked = false;
+                }
+                else if ( GUI.Button( new Rect( windowXVal + 150, windowYVal + 50, 100, 50 ), "Leave" ) )
+                {
+                   print( "You leave the door locked" );
+                }
+            }  
+        }
+        
+    }
+
+    private void OpenDoor()
+    {
+        thisDoor.transform.position += new Vector3( transformXVal, transformYVal, transformZVal );
+    }
+
+    private void CloseDoor()
+    {
+        thisDoor.transform.position -= new Vector3( transformXVal, transformYVal, transformZVal );
+    }
+
+    private void OnTriggerEnter( Collider other )
+    {
+        if ( other.tag == "Player" && door_is_locked )
+        {
+            encountered_locked_door = true;
+        }
+        else if ( other.tag == "Player" && !door_is_locked)
+        {
+            OpenDoor();
+            door_is_open = true;
+        }
+       
+    }
+
+    private void OnTriggerExit( Collider other )
+    {
+        if ( door_is_open )
+        {
+            CloseDoor();
+            door_is_open = false;
+        }
     }
 }

a905c88a8e52573bc27a08048eed7a9c0ac47b24
Fiona Soetrisno <fiona.soetrisno@gmail.com>
2020-02-26 22:57:07 -0800

    timeline markers

---
+++ Assets/Scripts/ToggleTimeline.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static  class ToggleTimeline
+{
+ 
+    public static void toggle()
+    {
+        Debug.Log("Wheeee");
+    }
+}

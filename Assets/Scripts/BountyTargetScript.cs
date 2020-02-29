37f54a5f5c2e052df3bc4bb04f59786361b9a430
aehoover <ashoover@calpoly.edu>
2020-02-29 12:50:03 -0800

    bounty target enters death animation when capture button is pressed and conditions for capture are met

--- Assets/Scripts/BountyTargetScript.cs
+++ Assets/Scripts/BountyTargetScript.cs
@@ -6,17 +6,22 @@ public class BountyTargetScript : MonoBehaviour
 {
     public GameObject capturePopup;
     public int hit_points = 3;
+    private Animator targetAnimator;
+    private bool capture_is_go = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        targetAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if ( Input.GetKeyDown( KeyCode.R ) )
+        {
+            capture_is_go = true;
+        }
     }
 
     private void OnTriggerStay( Collider other )
@@ -24,6 +29,12 @@ public class BountyTargetScript : MonoBehaviour
         if ( other.gameObject.CompareTag( "Player" ) && hit_points == 1 )
         {
             capturePopup.SetActive( true );
+
+            if ( capture_is_go )
+            {
+                targetAnimator.SetBool( "Death_b", true );
+                targetAnimator.SetInteger( "DeathType_int", 2 );
+            }
         }
     }
 

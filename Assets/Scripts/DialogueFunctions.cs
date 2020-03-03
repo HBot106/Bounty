ac3fa4f124effb62e855f02099351f0947b3d8ae
Fiona Soetrisno <fiona.soetrisno@gmail.com>
2020-03-03 01:09:20 -0800

    theres now a dialogue cutscene

---
+++ Assets/Scripts/DialogueFunctions.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Playables;
+
+public class DialogueFunctions : MonoBehaviour
+{
+    public PlayableDirector timeline;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeline = GetComponent<PlayableDirector>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void startTimeline()
+    {
+        Debug.Log("hweee");
+        Debug.Log(timeline);
+    }
+}

bd43a6bed0e42252692e5172c22816e05b35b8b7
aehoover <ashoover@calpoly.edu>
2020-03-09 14:26:39 -0700

    time bonus added to level 2

--- Assets/Scripts/InventoryManager.cs
+++ Assets/Scripts/InventoryManager.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class InventoryManager : MonoBehaviour
 {
-    public int num_keys = 1;
+    public int num_keys = 0;
     public float total_gold = 0;
-    public float time_bonus_gold = 0;
+    public float time_bonus_gold = 1000;
     public float capture_bonus_gold = 0;
 
     // Start is called before the first frame update

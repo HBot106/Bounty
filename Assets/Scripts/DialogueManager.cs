241c5e63b5d4a1cb4798a42c7463639a6c27a5e5
Fiona Soetrisno <fiona.soetrisno@gmail.com>
2020-03-11 15:53:14 -0700

    finished the cutscenes

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -1,6 +1,8 @@
 ﻿using UnityEngine;
+using System.Collections;
 using UnityEngine.Events;
 using UnityEngine.Playables;
+using UnityEngine.SceneManagement;
 
 public class DialogueManager : MonoBehaviour
 {
@@ -10,25 +12,23 @@ public class DialogueManager : MonoBehaviour
 
     private float counter_started = -1;
     private float counter_stopped = 0;
+    private GameObject fadeCanvas;
 
     private void Start()
     {
         timeline = GetComponent<PlayableDirector>();
+        fadeCanvas = transform.Find("FaderCanvas").gameObject;
 
     }
-
     public void togglePlayableDirector()
     {
         timeline.playableGraph.GetRootPlayable(0).SetSpeed(1);
         counter_started++;
-        Debug.Log("counter started: " + counter_started);
 
     }
 
     public void stopTimeline()
     {
-        Debug.Log("WHEEEEEE");
-        Debug.Log("counter stopped: " + counter_stopped);
             
         if (Mathf.Approximately(counter_started - 1, counter_stopped))
         {
@@ -37,4 +37,26 @@ public class DialogueManager : MonoBehaviour
         counter_stopped++;
 
     }
+
+    public void loadScene(int i)
+    {
+        SceneManager.LoadScene(i);
+        Debug.Log("LOADING SCENE...");
+    }
+
+    public void fade()
+    {
+        Debug.Log("fading...");
+        StartCoroutine(FadeToBlack());
+    }
+
+    IEnumerator FadeToBlack()
+    {
+        for (float ft = 1f; ft >= -0.1; ft -= 0.1f)
+        {
+            fadeCanvas.GetComponent<CanvasGroup>().alpha = 1 - ft;
+            yield return null;
+        }
+    }
+
 }
